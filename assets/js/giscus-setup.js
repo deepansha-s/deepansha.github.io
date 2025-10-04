ea3a695782c43ae18a984c15219d965f50b2a8ad
deepansha-s <32883609+deepansha-s@users.noreply.github.com>
2025-10-04 20:54:31 +0000

    Deploying to gh-pages from @ deepansha-s/deepansha-s.github.io@160ef2b59338e46e922dee9771cad92e063cc1a0 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "deepansha-s/deepansha.github.io",
+    "data-repo": "deepansha-s/deepansha-s.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
