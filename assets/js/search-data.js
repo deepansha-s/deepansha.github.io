fbb2e9c105c23820a5b7fe4cebee1e79e8238e92
deepansha-s <32883609+deepansha-s@users.noreply.github.com>
2025-10-05 03:30:43 +0000

    Deploying to gh-pages from @ deepansha-s/deepansha-s.github.io@6302f49640a4627ea25377f81d0d7ff706e9b325 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
+        },{id: "nav-research",
+          title: "research",
+          description: "research experiences.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
         },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
